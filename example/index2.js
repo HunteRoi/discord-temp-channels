2fe6dac2faa43d500dfc039f4b786938aa1096ef
Tinaël Devresse <32441291+HunteRoi@users.noreply.github.com>
2021-10-03 04:25:05 +0200

    feat: Support djs13

--- example/index2.js
+++ example/index2.js
@@ -1,25 +1,76 @@
-const { ClientWithTempManager } = require('../lib');
+const { Intents } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const synchronizeSlashCommands = require('discord-sync-commands');
+const { ClientWithTempManager, TempChannelsManagerEvents } = require('../lib');
 
-const client = new ClientWithTempManager();
+const client = new ClientWithTempManager({
+	intents: [Intents.FLAGS.GUILD_VOICE_STATES],
+});
+synchronizeSlashCommands(
+	client,
+	[
+		new SlashCommandBuilder()
+			.setName('createtext')
+			.setDescription(
+				'Create a text channel linked to the temporary voice channel and the owner'
+			),
+	],
+	{ guildId: 'GUILD_ID' }
+);
 
 client.on('ready', () => {
-  console.log('Connected!');
+	console.log('Connected!');
 
-  client.tempChannelsManager.registerChannel('CHANNEL_ID', {
-    childCategory: 'CATEGORY_ID',
-    childAutoDelete: true,
-    childAutoDeleteIfOwnerLeaves: false,
-    childVoiceFormat: (str, count) => `Example #${count} | ${str}`,
-    childVoiceFormatRegex: /^Example #\d+ \|/,
-    childTextFormat: (str, count) => `example-${count}_${str}`,
-    childTextFormatRegex: /^example-\d+_/i,
-  });
+	client.tempChannelsManager.registerChannel('CHANNEL_ID', {
+		childCategory: 'CATEGORY_ID',
+		childAutoDelete: true,
+		childAutoDeleteIfOwnerLeaves: false,
+		childVoiceFormat: (str, count) => `Example #${count} | ${str}`,
+		childVoiceFormatRegex: /^Example #\d+ \|/,
+		childTextFormat: (str, count) => `example-${count}_${str}`,
+		childTextFormatRegex: /^example-\d+_/i,
+	});
 });
 
-client.on('message', (msg) => {
-  if (msg.content.startsWith('!createText')) {
-    client.tempChannelsManager.emit('createText', msg);
-  }
+client.on('interactionCreate', async (interaction) => {
+	if (interaction.isCommand() && interaction.commandName === 'createtext') {
+		await interaction.deferReply({ ephemeral: true });
+		client.tempChannelsManager.emit(
+			TempChannelsManagerEvents.createText,
+			interaction
+		);
+	}
 });
+
+client.tempChannelsManager.on(
+	TempChannelsManagerEvents.textChannelCreate,
+	(textChannel, interaction) =>
+		interaction &&
+		interaction
+			.editReply(
+				`The text channel #${textChannel?.name ?? ''} has been created !`
+			)
+			.catch((err) => console.error(err))
+);
+
+client.tempChannelsManager.on(
+	TempChannelsManagerEvents.textChannelDelete,
+	(textChannel, interaction) =>
+		interaction &&
+		interaction
+			.editReply(
+				`The text channel #${textChannel?.name ?? ''} has been removed!`
+			)
+			.catch((err) => console.error(err))
+);
+
+client.tempChannelsManager.on(
+	TempChannelsManagerEvents.voiceNotExisting,
+	(interaction) =>
+		interaction &&
+		interaction
+			.editReply('You must be owner of a temporary voice channel to do that!')
+			.catch((err) => console.error(err))
+);
 
 client.login('TOKEN');
--- src/handlers/createText.ts
+++ src/handlers/createText.ts
@@ -1,39 +1,85 @@
-import { Message } from 'discord.js';
-import { TempChannelsManager } from '..';
+import {
+	OverwriteType,
+	TextChannel,
+	Permissions,
+	Constants,
+	GuildMember,
+	Interaction,
+} from 'discord.js';
 
-export const handleTextCreation = async (manager: TempChannelsManager, message: Message) => {
-  if (!message) return;
-  
-  const owner = message.member;
-  
-  const voiceChannel = message.member.voice.channel;
-  if (!voiceChannel || !manager.channels.some(p => p.children.some(c => c.voiceChannel.id === voiceChannel.id))) return;
-  const parent = manager.channels.find(p => p.children.some(c => c.voiceChannel.id === voiceChannel.id));
-  if (!parent) return;
+import { TempChannelsManagerEvents } from '../TempChannelsManagerEvents';
+import { TempChannelsManager } from '../TempChannelsManager';
 
-  const child = parent.children.find(c => c.voiceChannel.id === voiceChannel.id);
-  if (!child || child.owner.id !== owner.id) return;
+export const handleTextCreation = async (
+	manager: TempChannelsManager,
+	interaction: Interaction
+) => {
+	if (!manager || !interaction) return;
 
-  if (!child.textChannel) {
-    const count = parent.children.indexOf(child) + 1;
-    const newChannelName = parent.options.childTextFormat(owner.displayName, count);
+	const owner = interaction.member as GuildMember;
+	const voiceChannel = owner.voice.channel;
+	if (
+		!voiceChannel ||
+		!manager.channels.some((p) =>
+			p.children.some((c) => c.voiceChannel.id === voiceChannel.id)
+		)
+	) {
+		return manager.emit(
+			TempChannelsManagerEvents.voiceNotExisting,
+			interaction
+		);
+	}
 
-    const textChannel = await message.guild.channels.create(newChannelName, {
-      parent: parent.options.childCategory,
-      type: 'text',
-      permissionOverwrites: [{ id: owner.id, type: 'member', allow: 'MANAGE_CHANNELS' }]
-    });
-    child.textChannel = textChannel;
+	const parent = manager.channels.find((p) =>
+		p.children.some((c) => c.voiceChannel.id === voiceChannel.id)
+	);
+	if (!parent) return;
 
-    return manager.emit('textChannelCreate', textChannel);
-  }
+	const child = parent.children.find(
+		(c) => c.voiceChannel.id === voiceChannel.id
+	);
+	if (!child || child.owner.id !== owner.id) return;
 
-  if (child.textChannel) {
-    const textChannel = child.textChannel;
+	if (!child.textChannel) {
+		const count = parent.children.indexOf(child) + 1;
+		const newChannelName = parent.options.childTextFormat(
+			owner.displayName,
+			count
+		);
 
-    await textChannel?.delete();
-    child.textChannel = null;
+		const textChannel = (await interaction.guild.channels.create(
+			newChannelName,
+			{
+				parent: parent.options.childCategory,
+				type: Constants.ChannelTypes.GUILD_TEXT,
+				permissionOverwrites: [
+					{
+						id: owner.id,
+						type: Constants.OverwriteTypes[
+							Constants.OverwriteTypes.member
+						] as OverwriteType,
+						allow: Permissions.FLAGS.MANAGE_CHANNELS,
+					},
+				],
+			}
+		)) as TextChannel;
+		child.textChannel = textChannel;
 
-    return manager.emit('textChannelDelete', textChannel);
-  }
+		return manager.emit(
+			TempChannelsManagerEvents.textChannelCreate,
+			textChannel,
+			interaction
+		);
+	} else {
+		const textChannel = child.textChannel;
+		child.textChannel = null;
+
+		await textChannel?.delete();
+
+		return manager.emit(
+			TempChannelsManagerEvents.textChannelDelete,
+			textChannel,
+			interaction
+		);
+	}
 };
